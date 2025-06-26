a5c90b592a2d62e34d9bd132c4b338b8646c9444
Abhinay Purty <abhinayrrimil@gmail.com>
2025-06-26 07:51:50 +0000

    Added backstage templates

--- app/main.py
+++ app/main.py
@@ -1,17 +1,28 @@
 from flask import Flask, Response
-from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter
+from prometheus_client import start_http_server, Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+import time
 
 app = Flask(__name__)
-REQUEST_COUNT = Counter("app_requests_total", "Total HTTP requests")
+
+# Custom metrics
+REQUEST_COUNT = Counter("request_count", "Total number of requests")
+REQUEST_LATENCY = Histogram("request_latency_seconds", "Request latency")
+
 
 @app.route("/")
-def index():
+def home():
     REQUEST_COUNT.inc()
-    return "Hello from abhinay-ms!"
+    with REQUEST_LATENCY.time():
+        time.sleep(0.1)  # simulate processing
+    return "Hello from Abhinay Mircoservices!"
 
+
+# Expose Prometheus metrics at /metrics
 @app.route("/metrics")
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port= 8080 )
+    app.run(host="0.0.0.0", port=5000)
+
